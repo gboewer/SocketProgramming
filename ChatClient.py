e97e72e089cf5664a2fdaa2367d92bc14a9234a7
Gil Boewer <Boewer.gil@gmail.com>
2022-04-21 11:14:56 +0200

    Update ChatClient.py

--- ChatClient.py
+++ ChatClient.py
@@ -5,23 +5,33 @@ import time
 SERVERIP = "143.47.184.219"
 SERVERPORT = 5378 
 SERVERADDRESS = (SERVERIP, SERVERPORT)
-SOCKETLISTENSIZE = 4096
-
 
 def sendMsg(recipient, msg):
     apiMsg = "SEND {} {}\n".format(recipient, msg)
     sock.sendall(apiMsg.encode("utf-8"))
 
+def rcvMsg():
+    str = ''
+    while(True):
+        str += sock.recv(1).decode("utf-8")
+        if(str[len(str) - 1] == '\n'):
+            break
+
+    return str
+
+def handshake(username):
+    req = "HELLO-FROM {}\n".format(username)
+    sock.sendall(req.encode("utf-8"))
+
 def login():
     usernameAccepted = False
     while not usernameAccepted:
+        connectSocket()
         username = input("Please enter a username: ")
-
-        
         req = "HELLO-FROM {}\n".format(username)
         sock.sendall(req.encode("utf-8"))
 
-        res = sock.recv(SOCKETLISTENSIZE).decode("utf-8")
+        res = rcvMsg()
         if(res == "IN-USE\n"):
             print("Username is already in use, please choose a different one.")
         elif(res == "BUSY\n"):
@@ -36,17 +46,6 @@ def login():
             print("Invalid input. Please try again.")
         elif(not res):
             print("Connection terminated. Socket is closed.")
-            sock.close()
-            connected = False
-            # sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            while not connected:
-                try: 
-                    sock.connect(SERVERADDRESS)
-                    connected = True
-                    print("re-connection successful. Please try again.")
-                except:
-                    time.sleep(2)
-
 
 def printUserList():
     sock.sendall("WHO\n".encode("utf-8"))
@@ -54,7 +53,7 @@ def printUserList():
 
 def receiveMessages(): 
     while(True):
-        res = sock.recv(SOCKETLISTENSIZE).decode("utf-8")
+        res = rcvMsg()
         
         if(res.split(" ", 1)[0] == "WHO-OK"):
             usernames = res.split(" ", 1)[1]
@@ -69,18 +68,18 @@ def receiveMessages():
         elif(res == "UNKNOWN\n"):
             print("Failed to send message: Unknown recipient\n")
 
-
-if __name__ == '__main__':
+def connectSocket():
+    global sock
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(SERVERADDRESS)
 
-    rec = threading.Thread(target=receiveMessages, daemon=True)
-
+if __name__ == '__main__':
+    connectSocket()
     try:
         login()        
 
         running = True
-
+        rec = threading.Thread(target=receiveMessages, daemon=True)
         rec.start()
 
         print("""\nCommands:\n- !who: provides a list of users that are currenty online\n- @<recipient> <message>: send a message to a recipient thats currently online\n- !quit: quit the chat client\n""")
@@ -89,7 +88,7 @@ if __name__ == '__main__':
             time.sleep(0.3)
             cmd = input("\nCommand: ")
             if cmd == "!who":
-                printUserList()
+                sock.sendall("WHO\n".encode("utf-8"))
             elif cmd[0] == '@':
                 recipient = cmd.split()[0][1:]
                 msg = cmd.split(' ', 1)[1]
@@ -102,5 +101,5 @@ if __name__ == '__main__':
     except OSError as err:
         print(err)
 
-    except Exception:
-        pass
+    except Exception as ex:
+        print(ex)
